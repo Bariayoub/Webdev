bf13c6897eafac15ef13efba8c3b6ab8319829cf
Bariayoub <>
2019-10-08 17:49:26 +0100

    javascript v1.4

--- code.js
+++ code.js
@@ -1,14 +1,33 @@
-function vecteur(a,b) {
+// function vecteur(a,b) {
+// 	this.a = a;
+// 	this.b = b;
+// }
+// vecteur.prototype.mod = function() {
+// 	return Math.sqrt(this.a*this.a + this.b*this.b);
+// };
+
+// vecteur.prototype.egale = function(v) {
+// 	return (this.a == v.a && this.b==v.b);
+// };
+
+
+
+
+
+class vecteur{
+	constructor(a,b){
 	this.a = a;
 	this.b = b;
-}
-vecteur.prototype.mod = function() {
-	return Math.sqrt(this.a*this.a + this.b*this.b);
-};
+	}
+	mod() {
+		return Math.sqrt(this.a*this.a + this.b*this.b);
+	}
 
-vecteur.prototype.egale = function(v) {
-	return (this.a == v.a && this.b==v.b);
-};
+	egale(v) {
+		return (this.a == v.a && this.b==v.b);
+	}
+
+}
 
 let x = new vecteur(2,2);
 let y = new vecteur(2,4);
@@ -16,4 +35,5 @@ let y = new vecteur(2,4);
 console.log(x.mod());
 console.log(y.mod());
 console.log(x.egale(y));
+
 
