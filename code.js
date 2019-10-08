5616ffcaa0a67433739bc418089f0fc1eb769e81
Bariayoub <>
2019-10-08 17:43:26 +0100

    javascript v1.3

--- code.js
+++ code.js
@@ -1,10 +1,19 @@
-function user(nom,prenom) {
-	this.nom = nom;
-	this.prenom = prenom;
-	this.direHi = function(){
-		console.log("HI");
-	};
+function vecteur(a,b) {
+	this.a = a;
+	this.b = b;
+}
+vecteur.prototype.mod = function() {
+	return Math.sqrt(this.a*this.a + this.b*this.b);
 };
 
-let u1 = new user("ghoudan","ayoub");
-let u2 = new user("bari","ayoub");
+vecteur.prototype.egale = function(v) {
+	return (this.a == v.a && this.b==v.b);
+};
+
+let x = new vecteur(2,2);
+let y = new vecteur(2,4);
+
+console.log(x.mod());
+console.log(y.mod());
+console.log(x.egale(y));
+
