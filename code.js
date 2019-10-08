438c58a064b7bd1282f8408a377f453d5356e588
Bariayoub <>
2019-10-08 18:04:45 +0100

    javascript v1.5

--- code.js
+++ code.js
@@ -14,26 +14,31 @@
 
 
 
-class vecteur{
-	constructor(a,b){
-	this.a = a;
-	this.b = b;
-	}
-	mod() {
-		return Math.sqrt(this.a*this.a + this.b*this.b);
-	}
+// class vecteur{
+// 	constructor(a,b){
+// 	this.a = a;
+// 	this.b = b;
+// 	}
+// 	mod() {
+// 		return Math.sqrt(this.a*this.a + this.b*this.b);
+// 	}
 
-	egale(v) {
-		return (this.a == v.a && this.b==v.b);
-	}
+// 	egale(v) {
+// 		return (this.a == v.a && this.b==v.b);
+// 	}
 
+// }
+
+// let x = new vecteur(2,2);
+// let y = new vecteur(2,4);
+
+// console.log(x.mod());
+// console.log(y.mod());
+// console.log(x.egale(y));
+
+
+let prenoms =['yasser', 'adnane' , 'anas', 'hiba'];
+
+for(let valeur of prenoms){
+	console.log(valeur);
 }
-
-let x = new vecteur(2,2);
-let y = new vecteur(2,4);
-
-console.log(x.mod());
-console.log(y.mod());
-console.log(x.egale(y));
-
-
