32050708709d7d9c785f5a9a59f31970bd04c88f
Bariayoub <>
2019-10-29 14:31:18 +0000

    web29-10-2019

---
+++ javascript/mousetracker.js
@@ -0,0 +1,24 @@
+const doc = document.body;
+const circle = document.querySelector('.circle');
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+function mouseCoordinates(e) {
+    var horizontalPosition = windowWidth - e.clientX - 15;
+    var verticalPosition= windowHeight - e.clientY - 15;
+    console.log(horizontalPosition);
+    console.log(verticalPosition);
+    
+    circle.style.left = horizontalPosition + 'px';
+    circle.style.top = verticalPosition + 'px';
+}
+
+function changeColorOnTouch() {
+    circle.style.backgroundColor = "orange";
+    circle.style.borderColor = "orange";
+}
+
+doc.addEventListener('mousemove', mouseCoordinates);
+circle.addEventListener('mouseenter', changeColorOnTouch);
+circle.addEventListener('mouseleave', function(){circle.removeAttribute("style");});
